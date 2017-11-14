c5b42d5d09c6a386775cf4689226b1becc2a61bf
GuillaumeCz <guillaume.couzy@gmail.com>
2017-11-14 20:57:51 +0100

    Generate Page DAO tests

---
+++ app/src/androidTest/java/fr/utt/if26/mytravel/DAO/PageDAOTest.java
@@ -0,0 +1,68 @@
+package fr.utt.if26.mytravel.DAO;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by paf on 14/11/17.
+ */
+public class PageDAOTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void insertRow() throws Exception {
+
+    }
+
+    @Test
+    public void deleteRow() throws Exception {
+
+    }
+
+    @Test
+    public void getRow() throws Exception {
+
+    }
+
+    @Test
+    public void getList() throws Exception {
+
+    }
+
+    @Test
+    public void itemToObject() throws Exception {
+
+    }
+
+    @Test
+    public void updateRow() throws Exception {
+
+    }
+
+    @Test
+    public void getList1() throws Exception {
+
+    }
+
+    @Test
+    public void insertRow1() throws Exception {
+
+    }
+
+    @Test
+    public void deleteRow1() throws Exception {
+
+    }
+
+}
